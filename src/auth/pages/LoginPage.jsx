6926b2a489d708fd27444e054c405688860aa073
mauridevcde <mauridevcde@gmail.com>
2022-11-11 16:40:22 -0300

    culminamos la seccion 15

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -8,8 +8,14 @@ export const LoginPage = () => {
   const navigate = useNavigate();
   
   const onLogin = () => {
+
+    const lastPath = localStorage.getItem("lastPath") || "/";
+    
     login('mauridevcde');
-    navigate('/', { replace: true })
+
+
+
+    navigate(lastPath, { replace: true })
   }
   return (
     <>
